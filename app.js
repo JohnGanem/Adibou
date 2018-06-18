9410df2dae13fb4231514a4cf20666e44211cb69
grand <grand@DESKTOP-A28RCDK>
2018-06-18 16:35:35 +0200

    bootstrap

--- app.js
+++ app.js
@@ -41,6 +41,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
 
 app.all("*", function (req, res, next) {
     var request = req.params[0];
