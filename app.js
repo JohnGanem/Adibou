4bc77f91257968eb56e8dd7a64a8dfe935b55f4a
grand <grand@DESKTOP-A28RCDK>
2018-06-18 17:59:43 +0200

    bootstrap

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(__dirname + '/public'));
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
